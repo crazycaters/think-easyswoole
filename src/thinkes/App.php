f78c2abdc75ad396c419b94a8f52b5bf44d97bcc
sqwindows <sqwindows@qq.com>
2019-12-18 12:07:11 +0800

    20191218

---
+++ src/thinkes/App.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by crazyCater
+ * User: crazyCater
+ * Copyright: thinkEs
+ * Date: 2019/12/14 10:58
+ */
+declare (strict_types=1);
+
+namespace thinkes;
+
+
+/**
+ * App 基础类
+ */
+class App
+{
+
+    /**
+     * 版本号
+     * @var string
+     */
+    const VERSION = '0.0.1';
+
+    /**
+     * 架构方法
+     * App constructor.
+     */
+    public function __construct()
+    {
+
+
+    }
+
+}
